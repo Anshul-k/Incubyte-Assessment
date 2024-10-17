83b51ffde07624cd426b04c5ea3d3334575b937a
Anshul Kasana <anshul.kasana98@gmail.com>
2024-10-17 13:06:03 +0530

    Function working for two numbers, normal and custom delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,3 +1,14 @@
 export const add = (numbers) => {
   if (!numbers) return 0;
+
+  let delimiter = /,|\n/; //comma and newline Delimiter (Most common)
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n", 2);
+    delimiter = new RegExp(parts[0].slice(2)); // For custom delimiter
+    numbers = parts[1];
+  }
+
+  const numArray = numbers.split(delimiter).map(Number);
+
+  return numArray.reduce((sum, num) => sum + num, 0);
 };
--- src/test/stringCalculator.test.js
+++ src/test/stringCalculator.test.js
@@ -3,3 +3,19 @@ import { add } from "../stringCalculator";
 it("should return 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+it("should return the number itself if only one number is provided", () => {
+  expect(add("1")).toBe(1);
+});
+
+it("should return the sum of two comma-separated numbers", () => {
+  expect(add("1,2")).toBe(3);
+});
+
+it("should handle new lines as delimiters", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
+
+it("should support custom delimiters", () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
